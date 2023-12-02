bca1763dea2dd23652183009455e197553a21e70
kadoshita <sublimer.me@gmail.com>
2023-12-02 11:40:20 +0900

    Add drawflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Drawflow from 'drawflow';
+import 'drawflow/dist/drawflow.min.css';
+import './app.css';
+import { drawflowData } from './data';
 
 export function App() {
-    return <h1>Hello world!</h1>;
+    const drawflowElement = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const editor = new Drawflow(drawflowElement.current);
+        editor.start();
+        editor.import(drawflowData);
+    }, []);
+
+    return <div id="drawflow" ref={drawflowElement}></div>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import React, { StrictMode } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
